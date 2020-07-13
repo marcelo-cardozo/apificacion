fc05fa8ebb966287dc08eca1b8660d4278e8c813
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-07-13 09:11:04 -0400

    Commit inicial

---
+++ src/main/java/com/marcelo/apificacion/ApificacionApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.apificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApificacionApplication.class, args);
+	}
+
+}
