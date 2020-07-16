019fc00a292e19348057e83dd7673a4e8a61aa4e
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-07-16 18:14:28 -0400

    Agregar comentarios a endpoint

--- src/main/java/com/marcelo/apificacion/config/WebConfig.java
+++ src/main/java/com/marcelo/apificacion/config/WebConfig.java
@@ -2,10 +2,7 @@ package com.marcelo.apificacion.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
--- src/main/java/com/marcelo/apificacion/controller/ConsultaController.java
+++ src/main/java/com/marcelo/apificacion/controller/ConsultaController.java
@@ -2,8 +2,15 @@ package com.marcelo.apificacion.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.marcelo.apificacion.exception.BadRequestException;
+import com.marcelo.apificacion.exception.ErrorResponse;
 import com.marcelo.apificacion.exception.RecordNotFoundException;
 import com.marcelo.apificacion.service.IAseguradoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,26 +25,40 @@ import java.util.Optional;
 @RequestMapping(path = "/consulta")
 @CrossOrigin("*")
 public class ConsultaController {
+    private static final String cedulaPattern = "[0-9]*";
 
     private IAseguradoService aseguradoService;
 
-    public ConsultaController (IAseguradoService aseguradoService){
+    public ConsultaController(IAseguradoService aseguradoService) {
         this.aseguradoService = aseguradoService;
     }
 
+    @Operation(summary = "Obtiene los datos de una persona que posee IPS")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "La persona posee IPS",
+                    content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "400", description = "Formato incorrecto de la cédula",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Persona no posee IPS",
+                    content = @Content)})
     @GetMapping(
             value = "/{cedula}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<ObjectNode> consultaAsegurado(@PathVariable(value = "cedula") String cedula) {
+    @ResponseStatus(HttpStatus.OK)
+    public ObjectNode consultaAsegurado(
+            @Parameter(description = "Cédula de la persona",
+                    example = "1234567",
+                    content = @Content(schema = @Schema(pattern = cedulaPattern)))
+            @PathVariable(value = "cedula") String cedula) {
         // Si la cedula contiene algun caracter que no sea un numero, tirar un BadRequest
-        if (!cedula.matches("[0-9]*")) {
+        if (!cedula.matches(cedulaPattern)) {
             throw new BadRequestException();
         }
 
         // Si se obtiene exitosamente los datos del asegurado, retornar los mismos, si no, tirar un RecordNotFound
         Optional<ObjectNode> asegurado = aseguradoService.findAseguradoByCi(cedula);
         if (asegurado.isPresent()) {
-            return new ResponseEntity<>(asegurado.get(), HttpStatus.OK);
+            return asegurado.get();
         } else {
             throw new RecordNotFoundException(String.format("usuario con cédula %s no existe", cedula));
         }
