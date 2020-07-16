eebd7e40235c01cc47cb950d3a21157d3333fdfb
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-07-16 18:12:57 -0400

    Retornar ErrorResponse on exception

--- src/main/java/com/marcelo/apificacion/exception/CustomExceptionHandler.java
+++ src/main/java/com/marcelo/apificacion/exception/CustomExceptionHandler.java
@@ -6,52 +6,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     /*
     * Metodo que maneja las excepciones especificas de tipo RecordNotFoundException tiradas en el RestController
     * */
     @ExceptionHandler(RecordNotFoundException.class)
-    protected ResponseEntity<Object> handleRecordNotFound(RecordNotFoundException ex) {
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode objectNode = mapper.createObjectNode();
-
-        objectNode.put("codigo", "g100");
-        objectNode.put("error", ex.getMessage());
-
-        return new ResponseEntity(objectNode, HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ResponseEntity<ErrorResponse> handleRecordNotFound(RecordNotFoundException ex) {
+        return new ResponseEntity(new ErrorResponse("g100", ex.getMessage()),
+                HttpStatus.NOT_FOUND);
     }
 
     /*
      * Metodo que maneja las excepciones especificas de tipo BadRequestException tiradas en el RestController
      * */
     @ExceptionHandler(BadRequestException.class)
-    protected ResponseEntity<Object> handleBadRequest(BadRequestException ex) {
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode objectNode = mapper.createObjectNode();
-
-        objectNode.put("codigo", "g101");
-        objectNode.put("error", "Parámetros inválidos");
-
-        return new ResponseEntity(objectNode, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException ex) {
+        return new ResponseEntity(new ErrorResponse("g101", "Parámetros inválidos"),
+                HttpStatus.BAD_REQUEST);
     }
 
     /*
      * Metodo que maneja otro tipo de excepciones tiradas en el RestController
      * */
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ObjectNode> handleAllOtherExceptions(Exception ex, WebRequest request) {
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode objectNode = mapper.createObjectNode();
-
-        objectNode.put("codigo", "g102");
-        objectNode.put("error", "Error interno del servidor");
-
-        return new ResponseEntity(objectNode, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ResponseEntity<ErrorResponse> handleAllOtherExceptions(Exception ex, WebRequest request) {
+        return new ResponseEntity(new ErrorResponse("g102", "Error interno del servidor"),
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
